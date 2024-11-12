38d25e4d113eb528c01c9a8a9a0d40e98c86b0a4
Dante620 <20213996@aloe.ulima.edu.pe>
2024-11-12 09:56:08 -0500

    Anadiendo el esqueleto

---
+++ ProyectodeToys/src/App.jsx
@@ -0,0 +1,68 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <header>
+          <a href='/App.jsx'><img src="../imagenes/Imagen1.jpg"></img></a>
+          <ul>
+            <li>📁 CATEGORÍAS </li>
+            <li>🔜 PRE-VENTA </li>
+            <li>💰 MASTER POINTS</li>
+            <li>🤖 CLUB TOYS MASTER </li>
+            <li>🙂 RESEÑAS </li>
+          </ul>
+          <ul>
+            <li><img src="../imagenes/Imagen29.jpg"></img></li>
+            <li><img src="../imagenes/Imagen30.jpg"></img></li>
+            <li><img src="../imagenes/Imagen31.jpg"></img></li>
+            <li><img src="../imagenes/Imagen32.jpg"></img></li>
+          </ul>
+      </header>
+      <div>
+
+      </div>
+      <div>
+        <h2>NOVEDADES</h2>
+      </div>
+      <div>
+        <button>CATEGORIAS</button>
+      </div>
+      <div>
+        <h2>PRODUCTOS A MENOS DE S/.100</h2>
+      </div>
+      <div>
+      <button>VER MÁS</button>
+      </div>
+      <div>
+          <aside>
+            <h3>Tambien puedes recibir informacion por correo Electronico</h3>
+            <br/>
+            <input type="text"></input><button>Enviar</button>
+
+          </aside>
+      </div>
+      <div>
+        <h2>PRODUCTOS MAS VENDIDOS</h2>
+      </div>
+      <div>
+        <button>VER MAS</button>
+      </div>
+      <div>
+        <h2>PRE-VENTAS</h2>
+      </div>
+      <div>
+        <button>VER MAS</button>
+      </div>
+      <div></div>
+      <fooder>
+        <p>
+        © 2024, <a href='/App.jsx'>Toys Master </a><a href='https://www.shopify.com/es?utm_campaign=poweredby&utm_medium=shopify&utm_source=onlinestore'>Tecnología de Shopify</a>
+        </p>
+      </fooder>
+    </>
+  )
+}
+
+export default App
---
+++ ProyectodeToys/src/components/Sesion.jsx
@@ -0,0 +1,28 @@
+const Sesion = () => {
+    return
+    <>
+    <header>
+    <a href='/App.jsx'><img src="../imagenes/Imagen1.jpg"></img></a>
+    <ul>
+      <li>📁 CATEGORÍAS </li>
+      <li>🔜 PRE-VENTA </li>
+      <li>💰 MASTER POINTS</li>
+      <li>🤖 CLUB TOYS MASTER </li>
+      <li>🙂 RESEÑAS </li>
+    </ul>
+    <ul>
+      <li><img src="../imagenes/Imagen29.jpg"></img></li>
+      <li><img src="../imagenes/Imagen30.jpg"></img></li>
+      <li><img src="../imagenes/Imagen31.jpg"></img></li>
+      <li><img src="../imagenes/Imagen32.jpg"></img></li>
+    </ul>
+    </header>
+    <div>
+        <h1>Inicio de sesion</h1>
+        <input type="text"></input>
+        <input type="text"></input>
+        <a href="">¿Olvidaste tu contraseña?</a>
+    </div>
+
+</>
+}
